0911267c52c42e5bb942ad2ae0ff9a25083f6378
Revertigo <dekel241@gmail.com>
2020-11-15 01:49:23 +0200

    Add script to handle collisions on borders

---
+++ Assets/Scripts/3-collisions/OnCollideDestroy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCollideDestroy : MonoBehaviour
+{
+    [Tooltip("Every object tagged with this tag will trigger the destruction of this object")]
+    [SerializeField] string destroyTag;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == destroyTag && enabled)
+        {
+            //Destory the collide object
+            Destroy(other.gameObject);
+        }
+    }
+}
