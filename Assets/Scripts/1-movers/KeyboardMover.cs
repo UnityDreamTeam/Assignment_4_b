f7d5bbae67dc62b776ec5586f23d70b9bfcc91ed
Revertigo <dekel241@gmail.com>
2020-11-14 14:05:49 +0200

    Add borders with box colliders and adjust player's movement to use rigid body

--- Assets/Scripts/1-movers/KeyboardMover.cs
+++ Assets/Scripts/1-movers/KeyboardMover.cs
@@ -1,25 +1,22 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 /**
  * This component moves its object when the player clicks the arrow keys.
+ * It's movement is based on rigitbody components in order to use natural physics
+ * when colliding walls objects
  */
 public class KeyboardMover: MonoBehaviour {
     [Tooltip("Speed of movement, in meters per second")]
     [SerializeField] float speed = 1f;
+    [SerializeField] Rigidbody2D rb2D;
 
-    void Update() {
+    void FixedUpdate()
+    {
         float horizontal = Input.GetAxis("Horizontal"); // +1 if right arrow is pushed, -1 if left arrow is pushed, 0 otherwise
         float vertical = Input.GetAxis("Vertical");     // +1 if up arrow is pushed, -1 if down arrow is pushed, 0 otherwise
-        Vector3 movementVector = new Vector3(horizontal, vertical, 0) * speed * Time.deltaTime;
-        transform.position += movementVector;
-        //transform.Translate(movementVector);
-        // NOTE: "Translate(movementVector)" uses relative coordinates - 
-        //       it moves the object in the coordinate system of the object itself.
-        // In contrast, "transform.position += movementVector" would use absolute coordinates -
-        //       it moves the object in the coordinate system of the world.
-        // It makes a difference only if the object is rotated.
+        Vector2 movementVector = new Vector2(horizontal, vertical) * speed * Time.fixedDeltaTime;
+
+        //Update rigidbody position based on movement vector
+        rb2D.MovePosition(rb2D.position + movementVector + rb2D.velocity * Time.fixedDeltaTime);
     }
 }
