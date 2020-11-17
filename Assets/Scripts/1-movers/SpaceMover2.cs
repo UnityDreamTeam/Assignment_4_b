82b6703141eedbc07815b17520beed549fb0b194
Revertigo <dekel241@gmail.com>
2020-11-17 03:01:10 +0200

    Add circular border scene with right to left circular motion

---
+++ Assets/Scripts/1-movers/SpaceMover2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceMover2 : MonoBehaviour
+{
+    [SerializeField] Transform OriginalObject;
+
+    void LateUpdate()
+    {
+        //Get the actual position of the original
+        Vector3 pos = OriginalObject.position;
+
+        float HalfScreenSizeX = CalculateHalfScreenSize();
+
+        //Add the screen size
+        pos.x += HalfScreenSizeX * 2;
+
+        // wrap around if necessary
+        if (pos.x > HalfScreenSizeX * 2)
+            pos.x -= HalfScreenSizeX * 4;
+
+        transform.position = pos;
+    }
+    float CalculateHalfScreenSize()
+    {
+        float aspect = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().aspect;
+        float ortoSize = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().orthographicSize;
+        return aspect * ortoSize;
+    }
+}
